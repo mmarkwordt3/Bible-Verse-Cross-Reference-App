6a2c5bf90fe192124a28ab7f24730683fa24522b
mmarkwordt3 <michael.markwordt@gmail.com>
2026-07-30 10:59:09 -0400

    ci: bootstrap production data from source checkout

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+import js from '@eslint/js'; import tseslint from 'typescript-eslint'; export default tseslint.config({ignores:['dist/**','public/data/**','reports/**','.cache/**']},js.configs.recommended,...tseslint.configs.recommended,{files:['**/*.mjs'],rules:{'no-undef':'off'}});
